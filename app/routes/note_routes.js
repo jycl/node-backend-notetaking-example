a3c942b98ae4cb0c098cf123713f41df28700c17
Joshua Leung <joshuaycleung@gmail.com>
2018-08-30 18:05:58 +0800

    Add READ route for notes and import ObjectID from mongodb library

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,5 +1,21 @@
+var ObjectID = require('mongodb').ObjectID;
+
 //In Express, routes are wrapped in a function which takes Express instance and db as arguments
 module.exports = function(app, db) {
+    //READ route (using GET method and note Id)
+    app.get('/notes/:id', (req, res) => {
+        //Note that MongoDB requires not just an ID as a string but as an object
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id)};
+        db.collection('notes').findOne(details, (err, item) => {
+            if(err) {
+                res.send({'error':'An error has occured'});
+            } else {
+                res.send(item);
+            }
+        })
+    });
+
     app.post('/notes', (req, res) => {
         const note = {text: req.body.body, title: req.body.title};
         db.collection('notes').insert(note, (err, result) => {
